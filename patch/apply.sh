5984eaa06cbd3eb66f5df9ebff67074e7e2bf67a
psyafter <psyafter@gmail.com>
2016-03-15 08:27:19 +0200

    prevent patching "!" repos
    Change-Id: I4d4d4f9d46a9ff2bfb90b6de93806647792623c4

--- patch/apply.sh
+++ patch/apply.sh
@@ -7,13 +7,16 @@ CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHNAME=$(basename "$i")
 	PATCHTARGET=$PATCHNAME
-	for j in $(seq 4); do
-		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
-	done
+    if [[ ${PATCHNAME:0:1} != "!" ]]
+    then
+		for j in $(seq 4); do
+			PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+			if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+		done
 
-	echo
-	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*.patch || exit 1
+		echo
+		echo applying $PATCHNAME to $PATCHTARGET
+		cd "$CMBASE/$PATCHTARGET" || exit 1
+		git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*.patch || exit 1
+    fi
 done
